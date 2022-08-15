fecac23a64d720eb6804ae1ca318c176e90d49bb
Godswill Kalu <messagelordwill@gmail.com>
2022-08-15 21:35:36 +0100

    added dictionary on views

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -9,3 +9,11 @@ def movie_list(request):
         
     }
     return JsonResponse(data)
+def movie_details(request,pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name':movie.name,
+        'description':movie.description
+        'active':movie.active
+    }
+    return JsonResponse(data)
