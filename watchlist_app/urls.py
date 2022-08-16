7aba547f782a9f382f812a5a7754932149263902
Godswill Kalu <messagelordwill@gmail.com>
2022-08-16 14:57:44 +0100

    Deleted urls.py from watchlist app

--- watchlist_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from watchlist_app.views import movie_details, movie_list
-
-urlpatterns = [
-    path('list/', movie_list,name='movie-list'),
-    path('<int:pk>', movie_details, name='movie-details'),
-    
-]
