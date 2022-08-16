652ad869c5d69bed86f013666bfcb619b218979b
Godswill Kalu <messagelordwill@gmail.com>
2022-08-16 14:56:30 +0100

    Added Api folder, and files(urls,views)

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from watchlist_app.views import movie_details, movie_list
+from watchlist_app.api.views import movie_details, movie_list
 
 urlpatterns = [
     path('list/', movie_list,name='movie-list'),
--- watchmate/settings.py
+++ watchmate/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'watchlist_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
