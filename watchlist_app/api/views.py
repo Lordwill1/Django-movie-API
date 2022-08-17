09bfdfeacb64b514214b0a216259692ed4bbb952
Godswill Kalu <messagelordwill@gmail.com>
2022-08-17 02:08:33 +0100

    Added some lines on api views and imported function based views from Django restframe site

---
+++ watchlist_app/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializer
+
+@api_view()
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies)
+    return Response(serializer.data)
+
+@api_view()
+def movie_details(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
